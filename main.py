de0ccf1c55ab4dd85c7149af5a46522f862ecb9f
Waqas Tariq Dar <giftonian@users.noreply.github.com>
2024-05-22 16:49:34 +0500

    Update main.py
    blank line whiteee space....

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ This module is a sample python script.
 def hello():
     """
     This function is a sample function. This description is just for testing linting workflows.
-    
     """
     print("hi")
 
