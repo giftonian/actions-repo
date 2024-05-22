9c1618846c3cef1701108a4b2ab12572f0543eaa
Waqas Tariq Dar <giftonian@users.noreply.github.com>
2024-05-22 15:57:57 +0500

    Update main.py
    adding docstring to file and function to resolve linting issue

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+"""
+This module is a sample python script.
+"""
+
 def hello():
+    """
+    This function is a sample function. This description is just for testing linting workflows.
+    
+    """
     print("hi")
 
 
